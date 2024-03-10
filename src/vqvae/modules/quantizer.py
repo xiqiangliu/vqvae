d7d75be36bd7007c45a75d26073f08d6b7fc271f
Xiqiang Liu <9440183+xiqiangliu@users.noreply.github.com>
2024-03-10 15:42:18 -0700

    Add EMA to Vector Quantization Layer

--- src/vqvae/modules/quantizer.py
+++ src/vqvae/modules/quantizer.py
@@ -1,18 +1,27 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 from einops import rearrange
 
 
 class Quantizer(nn.Module):
-    """Implementation of Vector Quantization Layer.
+    """Implementation of Vector Quantization Layer with EMA.
 
     Args:
         num_embeddings (int): Number of embeddings.
         embedding_dim (int): Dimension of the embeddings.
     """
 
-    def __init__(self, num_embeddings: int, embedding_dim: int):
+    cluster_size: nn.Parameter
+    embed_avg: nn.Parameter
+
+    def __init__(
+        self,
+        num_embeddings: int,
+        embedding_dim: int,
+        decay: float = 0.99,
+        eps: float = 1e-5,
+    ):
         super().__init__()
 
         self.embedding_dim = embedding_dim
@@ -22,6 +31,12 @@ class Quantizer(nn.Module):
         self.embedding.weight.data.uniform_(
             -1 / self.num_embeddings, 1 / self.num_embeddings
         )
+
+        # EMA Parameters
+        self.decay = decay
+        self.eps = eps
+        self.register_buffer("cluster_size", torch.zeros(self.num_embeddings))
+        self.register_buffer("embed_avg", self.embedding.weight.data.clone())
 
     def forward(self, inputs: torch.Tensor):
         """Forward pass of the quantizer.
@@ -40,9 +55,9 @@ class Quantizer(nn.Module):
 
         # Calculate the distance between the input and the embeddings: (x - e)^2 = x^2 - 2*x*e + e^2
         distances = (
-            torch.sum(inputs**2, dim=1, keepdim=True)
+            (inputs**2).sum(dim=1, keepdim=True)
             - 2 * inputs @ self.embedding.weight.T
-            + torch.sum(self.embedding.weight**2, dim=1)
+            + (self.embedding.weight**2).sum(dim=1)[None, ...]
         ).contiguous()
 
         # Find the closest embeddings
@@ -51,9 +66,40 @@ class Quantizer(nn.Module):
             self.embedding(encoding_indices), "b h w c -> b c h w"
         ).contiguous()
 
+        # Update embeddings usign EMA. Using it to avoid embedding collapse.
+        # Refer to the appendix of the VQ-VAE paper for more details.
+        if self.training:
+            # Update the cluster size
+            one_hot = (
+                F.one_hot(encoding_indices, self.num_embeddings)
+                .type(inputs.dtype)
+                .view(-1, self.num_embeddings)
+            )
+            cluster_size = one_hot.sum(dim=0)
+            self.cluster_size.data *= self.decay
+            self.cluster_size.data.add_(cluster_size, alpha=1 - self.decay)
+
+            # Update the average embedding
+            self.cluster_size.data = (
+                (self.cluster_size + self.eps)
+                / (self.cluster_size.sum() + self.num_embeddings * self.eps)
+                * self.cluster_size.sum()
+            )
+
+            self.embed_avg.data *= self.decay
+            self.embed_avg.add_((inputs.T @ one_hot).T, alpha=1 - self.decay)
+
+            # Update the embeddings
+            self.embedding.weight.data.copy_(
+                self.embed_avg / self.cluster_size.unsqueeze(1)
+            )
+
         # Calculate the quantization loss and commitment loss
         inputs = rearrange(inputs, "(b h w) c -> b c h w", b=b, h=h, w=w).contiguous()
         quantization_loss = torch.mean((quantized.detach() - inputs) ** 2)
         commitment_loss = torch.mean((quantized - inputs.detach()) ** 2)
+
+        # Ensure straight through gradient for the quantized tensor
+        quantized = inputs + (quantized - inputs).detach()
 
         return quantized, quantization_loss, commitment_loss
